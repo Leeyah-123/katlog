e971ad7689775caf6a6d11c72958c61bff413503
Leeyah-123 <junaidaaliyah260@gmail.com>
2025-01-01 18:28:52 +0100

    chore: Remove winston log files

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -14,8 +14,6 @@ const logger = winston.createLogger({
         winston.format.simple()
       ),
     }),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
